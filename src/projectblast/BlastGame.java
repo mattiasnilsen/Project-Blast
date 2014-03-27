8182350ac0e92896d139d5c83a63b546c37c646c
Kerp1 <kerp@live.se>
2014-03-27 22:04:34 +0100

    Added some code that starts the game.

--- src/projectblast/BlastGame.java
+++ src/projectblast/BlastGame.java
@@ -1,5 +1,6 @@
 package projectblast;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
@@ -10,15 +11,24 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class BlastGame extends StateBasedGame{
 
+    public static void main(String args[]) {
+        try {
+            AppGameContainer container = new AppGameContainer(new BlastGame("Blast Game"));
+            container.setDisplayMode(800, 600, false);
+            container.setTargetFrameRate(60);
+            container.start();
+        } catch (Exception e) { //TODO Proper exception handling. 
+            
+        }
+    }
+    
 	public BlastGame(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
-		// TODO Auto-generated method stub
-
+	    addState(new GameplayState());
 	}
 
 }
