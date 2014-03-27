fd6cf39d9a1e1c22f6cc51835a59d27d93e948c8
Axel Saven Östebo <axelostebo@gmail.com>
2014-03-27 13:25:17 +0100

    First commit with slick

---
+++ src/BlastGame.java
@@ -0,0 +1,19 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class BlastGame extends StateBasedGame{
+
+	public BlastGame(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
