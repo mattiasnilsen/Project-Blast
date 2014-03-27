4a5fff9ce9ec0b57cb99373cbfbba35bc9b78e0a
Kerp1 <kerp@live.se>
2014-03-27 21:38:04 +0100

    Added bomber hero class.

---
+++ src/projectblast/Bomber.java
@@ -0,0 +1,28 @@
+package projectblast;
+
+import org.newdawn.slick.Image;
+
+/**
+ * 
+ * @author Mattias Nilsen
+ *
+ */
+public class Bomber extends Hero {
+
+    public Bomber(int x, int y, Image sprite) {
+        super(x, y, sprite);
+    }
+
+    @Override
+    public void primaryAbility() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void secondaryAbility() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
