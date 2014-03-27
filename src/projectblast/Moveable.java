ac6bbc1f70af66ef6646e591c353e8f42562e180
Anton Freudenthaler <franton@student.chalmers.se>
2014-03-27 16:13:52 +0100

    Added move and place to Moveable

--- src/projectblast/Moveable.java
+++ src/projectblast/Moveable.java
@@ -1,5 +1,14 @@
 package projectblast;
 
+/**
+ * 
+ * @author franton
+ *	An interface for all Moveable things in projectblast.
+ */
 public interface Moveable {
-
+	
+	public void move(int dx, int dy);
+	
+	public void place(int x, int y);
+	
 }
