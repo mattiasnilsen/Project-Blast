f36f4647a6d6758414945f65517a79a2e46567dc
Anton Freudenthaler <franton@student.chalmers.se>
2014-03-28 09:44:46 +0100

    Added Mage class

---
+++ src/projectblast/Mage.java
@@ -0,0 +1,27 @@
+package projectblast;
+
+import org.newdawn.slick.Image;
+/**
+ * 
+ * @author franton
+ *
+ */
+public class Mage extends Hero {
+
+	public Mage(int x, int y, Image sprite) {
+		super(x, y, sprite);
+	}
+
+	@Override
+	public void primaryAbility() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void secondaryAbility() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
