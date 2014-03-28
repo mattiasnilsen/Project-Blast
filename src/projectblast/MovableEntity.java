09ec1d1b3bc4dc46043a7451679f4fed45bc8c1b
Mattias Nilsen <kerp@live.se>
2014-03-28 10:26:21 +0100

    Implemented new methods in Movable and Updatable.

--- src/projectblast/MovableEntity.java
+++ src/projectblast/MovableEntity.java
@@ -5,19 +5,19 @@ import org.newdawn.slick.Image;
 /**
  * 
  * @author franton
- *
+ * revised by Mattias Nilsen
  */
-public class MovableEntity extends Entity implements Movable {
+public class MovableEntity extends Entity implements Movable, Updatable {
 
+	private int speed = 4;
+	private Direction moveDirection = null;
+	
 	public MovableEntity(int x, int y, Image sprite) {
 		super(x, y, sprite);
 	}
-
-	private int speed = 4;
 	
 	public void move(int dx, int dy) {
 		place(getX() + dx, getY() + dy);
-		
 	}
 
 	
@@ -34,6 +34,31 @@ public class MovableEntity extends Entity implements Movable {
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+
+
+	@Override
+	public void move(Direction direction) {
+		place(getX() + (getSpeed() * direction.getX()), getY() + (getSpeed() * direction.getY()));
+	}
+
+
+	@Override
+	public void startMove(Direction direction) {
+		this.moveDirection = direction;
+	}
+
+
+	@Override
+	public void stopMove() {
+		moveDirection = null;
+	}
+
+	@Override
+	public void update() {
+		if(moveDirection != null) {
+			move(moveDirection);
+		}
 	}
 		
 	
