cc945015d0ca0d2561422492f94eff4f61460e9b
Mattias Nilsen <kerp@live.se>
2014-03-29 08:24:38 +0100

    Fixed so that a hero's bomb should be updated/drawn.

--- src/projectblast/Hero.java
+++ src/projectblast/Hero.java
@@ -1,20 +1,47 @@
 package projectblast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 public abstract class Hero extends MovableEntity{
 
-	int power;
-	int bombCount;
+	private int power;
+	private int bombCount;
+	private List<Explosive> explosives = new ArrayList<Explosive>();
 	
     public Hero(int x, int y, Image sprite, int speed, Animation[] animations) {
         super(x, y, sprite, speed, animations);
-
+    }
+    
+    public void addExplosive(Explosive explosive) {
+    	explosives.add(explosive);
+    }
+    
+    public void removeExplosive(Explosive explosive) {
+    	explosives.remove(explosive);
     }
 
+    @Override
+    public void update() {
+    	super.update();
+    	for(Explosive explosive : explosives) {
+    		explosive.update();
+    	}
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+    	super.draw(g);
+    	for(Explosive explosive : explosives) {
+    		explosive.draw(g);
+    	}
+    }
+    
 	public abstract void primaryAbility();
 		
 	public abstract void secondaryAbility();
-	
 }
