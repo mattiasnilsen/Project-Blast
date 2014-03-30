0c835d1f6f31b23b7b6e9bb9ea90d6867b988db6
Mattias Nilsen <kerp@live.se>
2014-03-30 19:56:44 +0200

    Added comments to Updatable.java

--- src/projectblast/Updatable.java
+++ src/projectblast/Updatable.java
@@ -2,8 +2,10 @@ package projectblast;
 /**
  * Common interface for all updatable classes in project blast.
  * @author Mattias Nilsen
- *
  */
 public interface Updatable {
+    /**
+     * Called on an object every tick to update it.
+     */
 	public void update();
 }
