e0aef4136eee024d526c712b0d86d019083bc715
Mattias Nilsen <kerp@live.se>
2014-03-30 19:48:29 +0200

    Added comments to Drawable.java

--- src/projectblast/Drawable.java
+++ src/projectblast/Drawable.java
@@ -1,17 +1,22 @@
-/**
- * 
- */
 package projectblast;
 
 import org.newdawn.slick.Color;
-
 import org.newdawn.slick.Graphics;
 
 /**
  * @author Mattias Nilsen
- *
+ * Common interface for every class that needs to be drawn on the screen.
  */
 public interface Drawable {
+    /**
+     * Draw this object at its position.
+     * @param g The graphics object used to draw.
+     */
 	public void draw(Graphics g);
+	/**
+	 * Draw this object at its position with the supplied color.
+	 * @param g The graphics object used to draw.
+	 * @param teamColor The color to user.
+	 */
 	public void draw(Graphics g, Color teamColor);
 }
