50379a5188b2445e3ddb425a139d72444356ab14
Anton Freudenthaler <franton@student.chalmers.se>
2014-03-31 17:32:13 +0200

    Added IBlastModel interface

---
+++ src/projectblast/IBlastModel.java
@@ -0,0 +1,19 @@
+package projectblast;
+
+import java.util.List;
+
+public interface IBlastModel {
+	
+	void left(int playerID);
+	void right(int playerID);
+	void up(int playerID);
+	void down(int playerID);
+	
+	void primary(int playerID);
+	void secondary(int playerID);
+	
+	/**
+	 * @return an unsorted list of entities.
+	 */
+	List<Entity> getEntities();
+}
