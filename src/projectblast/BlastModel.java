cfa6ec03f501e2f0d2d9c6f64219f5dfdd5dde8a
Anton Freudenthaler <franton@student.chalmers.se>
2014-03-31 17:50:03 +0200

    Added BlastModel

---
+++ src/projectblast/BlastModel.java
@@ -0,0 +1,61 @@
+package projectblast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import projectblast.Movable.Direction;
+
+public class BlastModel implements IBlastModel {
+	
+	List<Entity> entities;
+	ArrayList<Player> players;
+	
+	public BlastModel(ArrayList<Player> players){
+		entities = new LinkedList();
+		this.players = players;  
+	}
+	@Override
+	public void left(int playerID) {
+		players.get(playerID-1).getHero().move(Direction.WEST);
+
+	}
+
+	@Override
+	public void right(int playerID) {
+		players.get(playerID-1).getHero().move(Direction.EAST);
+
+	}
+
+	@Override
+	public void up(int playerID) {
+		players.get(playerID-1).getHero().move(Direction.NORTH);
+
+	}
+
+	@Override
+	public void down(int playerID) {
+		players.get(playerID-1).getHero().move(Direction.SOUTH);
+
+	}
+
+	@Override
+	public void primary(int playerID) {
+		players.get(playerID-1).getHero().primaryAbility();
+
+	}
+
+	@Override
+	public void secondary(int playerID) {
+		players.get(playerID-1).getHero().secondaryAbility();
+	}
+
+	@Override
+	public List<Entity> getEntities() {
+		
+		return entities;
+	}
+	
+	
+
+}
