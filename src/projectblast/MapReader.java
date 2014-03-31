5bd55d0636b37bd2bf1034e4fa27e8bf615784c3
Axel Savén Östebo <axelostebo@gmail.com>
2014-03-31 21:16:53 +0200

    Added a MapReader class to read maps and create entities

---
+++ src/projectblast/MapReader.java
@@ -0,0 +1,48 @@
+package projectblast;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+/**
+ * 
+ * @author Axel Savén Östebo
+ *
+ */
+public class MapReader {
+	private TiledMap map;
+	
+	MapReader(TiledMap map){
+		this.map = map;
+	}
+	
+	public List<Entity> createEntities(){
+		List<Entity> list = new LinkedList<Entity>();
+		
+		//Step through the map and read property values
+		for (int x = 0; x < map.getWidth(); x++){
+			for (int y = 0; y < map.getHeight(); y++){
+				//Fetch tile id and get its type, with air as default
+				String prop = map.getTileProperty(map.getTileId(x, y, 0), "type", "air");
+				if (!"air".equals(prop)){
+					if ("block".equals(prop)){
+						//TODO fix sprite here
+						list.add(new SolidBlock(x*Constants.TILE_SIZE,y*Constants.TILE_SIZE,null));
+					}
+					if ("box".equals(prop)){
+						//TODO fix sprite here
+						list.add(new DestructibleBlock(x*Constants.TILE_SIZE,y*Constants.TILE_SIZE,null));
+					}
+					if ("tower".equals(prop)){
+						//TODO fix sprite here
+						list.add(new Tower(x*Constants.TILE_SIZE,y*Constants.TILE_SIZE,null));
+					}
+				}
+			}
+		}
+		
+		return list;
+	}
+
+}
