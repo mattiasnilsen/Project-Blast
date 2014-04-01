a3e9768a9f691b60e4d0d528900eba1db1b76580
Axel Saven Östebo <axelostebo@gmail.com>
2014-04-01 13:20:35 +0200

    Added solidblock

---
+++ src/projectblast/SolidBlock.java
@@ -0,0 +1,15 @@
+package projectblast;
+
+import org.newdawn.slick.Image;
+/**
+ *
+ * @author Alex Tao
+ */
+public class SolidBlock extends Block{
+	
+	public SolidBlock(int x, int y, Image sprite) {
+		super(x, y, sprite);
+
+	}
+
+}
