b81d162d4bd653e951d9db335025f2e9b09dfc18
Axel Saven Östebo <axelostebo@gmail.com>
2014-04-03 12:04:53 +0200

    Made explosives generate a cross: No collision check yet

--- src/projectblast/model/Explosion.java
+++ src/projectblast/model/Explosion.java
@@ -1,4 +1,6 @@
 package projectblast.model;
+import java.util.List;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
 
@@ -6,8 +8,31 @@ import org.newdawn.slick.geom.Rectangle;
 
 public class Explosion extends Entity {
 
-	public Explosion(int x, int y, Image sprite) {
+	private int life = 60;
+	private List<Explosion> parts;
+	
+	public Explosion(int x, int y, Image sprite, int power) {
 		super(x, y, sprite, new Rectangle(x,y,32,32));
+		expand(power);
 	}
 
+	
+	public void update(){
+		life--;
+	}
+	
+	public boolean isDead(){
+		return life <= 0;
+	}
+	
+	private void expand(int pow){
+		for (int i = 0; i < pow; i++){
+			int size = Constants.TILE_SIZE;
+			parts.add(new Explosion(getX() + size,getY(),getSprite(),1));
+			parts.add(new Explosion(getX() - size,getY(),getSprite(),1));
+			parts.add(new Explosion(getX(),getY() + size,getSprite(),1));
+			parts.add(new Explosion(getX(),getY() - size,getSprite(),1));
+		}
+	}
+	
 }
