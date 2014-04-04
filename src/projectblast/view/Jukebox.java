a3f191357054ac6c42cd9e93592ceeb2fea51221
Axel Savén Östebo <axelostebo@gmail.com>
2014-04-04 15:51:27 +0200

    Added resources. Did some small tweaks here and there. Started working on Jukebox class.

--- src/projectblast/view/Jukebox.java
+++ src/projectblast/view/Jukebox.java
@@ -5,35 +5,44 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 public class Jukebox {
-	private static boolean initialized = false;
-	private static Sound SOUND_EXPLOSION, SOUND_DEATH, SOUND_PLACEBOMB, SOUND_FIREBALL;
-	private static Music MUSIC_TITLE, MUSIC_SETTINGS, MUSIC_BATTLE;
 	
-	public static void playSound(Sound s){
-		if (!initialized){
-			initialize();
+	public enum Sounds{
+		EXPLOSION("Boom.ogg"), DEATH("Boom.ogg"), PLACEBOMB("Noise.ogg"), FIREBALL("Noise.ogg");
+		
+		private final String folder = "data/sound/";
+		private final String soundFile;
+		
+		private Sounds(String s){
+			this.soundFile = s;
+		}
+		
+		public Sound getSound(){
+			try {
+				return new Sound(folder + soundFile);
+			} catch (SlickException e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 	}
 	
-	private static void initialize(){
-		initialized = true;
-		try {
-			//Sound
-			SOUND_EXPLOSION = new Sound("data/sound/Boom.ogg");
-			SOUND_DEATH     = new Sound("data/sound/Boom.ogg");
-			SOUND_PLACEBOMB = new Sound("data/sound/Noise.ogg");
-			SOUND_FIREBALL  = new Sound("data/sound/Noise.ogg");
-			
-			//Music
-			MUSIC_TITLE    = new Music("data/music/GarageTownDifferent.ogg");
-			MUSIC_SETTINGS = new Music("data/music/GarageTownDifferent.ogg");
-			MUSIC_BATTLE   = new Music("data/music/DrumsInTheDeepV2.ogg");
-			
-			
-		} catch (SlickException e) {
-			System.out.println("One or more sounds not found!");
-			e.printStackTrace();
+	public enum Musics{
+		TITLE("DrumsInTheDeepV2.ogg"), SETTINGS("GarageTownDifferent.ogg"), BATTLE("DrumsInTheDeepV2.ogg");
+		
+		private final String folder = "data/music/";
+		private final String musicFile;
+		
+		private Musics(String s){
+			this.musicFile = s;
+		}
+		
+		public Sound getMusic(){
+			try {
+				return new Sound(folder + musicFile);
+			} catch (SlickException e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 	}
-	
 }
