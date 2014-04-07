c7c5d6f8046c512cb218f173d5a8c670902bf127
Axel Savén Östebo <axelostebo@gmail.com>
2014-04-07 12:42:44 +0200

    Removed tile and collisionbox

--- src/projectblast/control/Main.java
+++ src/projectblast/control/Main.java
@@ -1,18 +1,20 @@
 package projectblast.control;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
 
 import projectblast.model.Constants;
 
 public class Main {
+	
 	public static void main(String args[]) {
         try {
             AppGameContainer container = new AppGameContainer(new BlastGame("Project Blast!"));
             container.setDisplayMode(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, false);
             container.setTargetFrameRate(Constants.FRAMERATE);
             container.start();
-        } catch (Exception e) { //TODO Proper exception handling. 
-            
+        } catch (SlickException e) { //TODO Proper exception handling. 
+            e.printStackTrace();
         }
     }
 }
