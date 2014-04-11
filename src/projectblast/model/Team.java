6bd3ffc53f3953c4123ded450ddbbe628b7fc80c
Mattias Nilsen <kerp@live.se>
2014-04-12 00:46:57 +0200

    Added static method to get neutral team.

--- src/projectblast/model/Team.java
+++ src/projectblast/model/Team.java
@@ -17,6 +17,8 @@ public class Team {
 	private ArrayList<Player> playerList = new ArrayList<Player>();
 	private ArrayList<Tower> towerList = new ArrayList<Tower>();
 	private Color teamColor;
+	
+	private static Team neutralTeam = null;
 
 	public Team(String teamName, Color teamColor){
 		this.teamName = teamName;
@@ -34,8 +36,14 @@ public class Team {
 		this(teamName, startingTower, teamColor, player1);
 		playerList.add(player2);
 	}
+	//TODO Change implementation, maybe this is not best way to do it?
+	public static Team getNeutralTeam() {
+		if(neutralTeam == null) {
+			neutralTeam = new Team("Neutral", null, Color.white, null, null);
+		}
+		return neutralTeam;
+	}
 	
-
 	public void capturedTower(Tower tower){
 		towerList.add(tower);
 		addScore(10);
