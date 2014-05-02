74f4fac2ef3fae950f70a08e22ede0c7f07782b7
Mattias Nilsen <kerp@live.se>
2014-05-02 12:12:46 +0200

    Removed playerlists and towerlists from team, they were not used and are not needed.

--- src/projectblast/model/Team.java
+++ src/projectblast/model/Team.java
@@ -1,27 +1,19 @@
 package projectblast.model;
 
 import org.newdawn.slick.Color;
-import java.awt.List;
-import java.util.ArrayList;
 /**
  * 
  * @author Alex Tao
  *
  */
 public class Team {
-	
-	//TODO change colors of heroes and towers
-	
 	private String teamName;
-	private ArrayList<Player> playerList = new ArrayList<Player>();
-	private ArrayList<Tower> towerList = new ArrayList<Tower>();
 	private Color teamColor;
 	private final Side teamSide;
 	
 	public enum Side{
 		LEFT, RIGHT, NULL;
 	}
-	
 	
 	private static Team neutralTeam = null;
 
@@ -31,33 +23,11 @@ public class Team {
 		this.teamSide  = teamSide;
 	}
 	
-	public Team(String teamName, Tower startingTower, Side teamSide, Color teamColor,Player player1){
-		this(teamName, teamColor, teamSide);
-		playerList.add(player1);
-	
-		towerList.add(startingTower);
-	}
-	
-	public Team(String teamName, Tower startingTower, Side teamSide, Color teamColor,Player player1, Player player2){
-		this(teamName, startingTower, teamSide, teamColor, player1);
-		playerList.add(player2);
-	}
-	
-	//TODO Change implementation, maybe this is not best way to do it?
 	public static Team getNeutralTeam() {
 		if(neutralTeam == null) {
-			neutralTeam = new Team("Neutral", null, Side.NULL, Color.white, null, null);
+			neutralTeam = new Team("Neutral", Color.white, Side.NULL);
 		}
 		return neutralTeam;
-	}
-	
-	public void capturedTower(Tower tower){
-		towerList.add(tower);
-	}
-	
-	public void lostTower(Tower tower){
-		towerList.remove(tower);
-		
 	}
 	
 	public Color getColor(){
@@ -75,9 +45,5 @@ public class Team {
 
 	public Side getSide() {
 		return teamSide;
-	}
-	
-	
-	
-	
+	}	
 }
