539418e270685143e2d4b83f88e8748ecfdcb8bd
Atlanca <talex@student.chalmers.se>
2014-05-07 14:52:50 +0200

    Some tests

---
+++ src/projectblast/test/EntityTest.java
@@ -0,0 +1,53 @@
+package projectblast.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import projectblast.model.Position;
+import projectblast.model.SolidBlock;
+
+public class EntityTest {
+
+	@Test
+	public void testIsOnGrid() {
+
+		Position pos = new Position(96,96);
+		SolidBlock block = new SolidBlock(pos);
+		
+		Position pos2 = new Position(97,97);
+		SolidBlock block2 = new SolidBlock(pos2);
+		
+		assertTrue(block.isOnGrid() == (block.getX()/32.0 == 3 && block.getY()/32.0 == 3));
+		assertTrue(block2.isOnGrid() == (block2.getX()/32.0 == 3 && block2.getY()/32.0 == 3));
+	}
+
+	@Test
+	
+	public void testSetGetXY(){
+
+		Position pos = new Position(96,96);
+		SolidBlock block = new SolidBlock(pos);
+		
+		block.setX(100);
+		assertTrue(block.getX() == 100);
+		
+		block.setY(120);
+		assertTrue(block.getY() == 120);
+	
+	}
+	
+	@Test
+	public void testSetGetPosition(){
+		Position pos = new Position(96,96);
+		SolidBlock block = new SolidBlock(pos);
+		
+		block.setPosition(1,1);
+		
+		assertTrue(block.getX() == 1 && block.getY() == 1);
+		block.setPosition(pos);
+		assertTrue(block.getPosition().equals(pos));
+	}	
+	
+	
+}
---
+++ src/projectblast/test/MageTest.java
@@ -0,0 +1,74 @@
+package projectblast.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+import org.newdawn.slick.Color;
+
+import projectblast.model.*;
+import projectblast.model.Team.Side;
+import projectblast.model.Movable.*;
+import projectblast.model.explosive.Explosive;
+import projectblast.model.hero.*;
+import projectblast.model.powerups.AmmoPowerUp;
+
+public class MageTest {
+
+	@Test
+	public void testDestroy() {
+		Position pos = new Position(1,1);
+		int speed = 100;
+		Direction dir = Direction.EAST;
+		Team team = new Team("Test", Color.red, Side.LEFT );
+		Hero mage = new Mage(pos,speed,dir,team);
+		mage.destroy();
+		assertTrue(mage.isDestroyed());
+	}
+	@Test
+	public void testAddPowerUp(){
+		Position pos = new Position(1,1);
+		int speed = 100;
+		Direction dir = Direction.EAST;
+		Team team = new Team("Test", Color.red, Side.LEFT );
+		Hero mage = new Mage(pos,speed,dir,team);
+		AmmoPowerUp apu = new AmmoPowerUp();
+		
+		int ammo = mage.getAmmo();
+		assertTrue(mage.getAmmo() == 3);
+		
+		for (int i = 0; i < 3; i++){
+			mage.addPowerUp(apu);
+		}	
+		assertTrue(mage.getAmmo() == 6);
+		
+		for (int i = 0; i < 10; i++){
+			apu.reverse(mage);
+		}
+		//Ammo should never be allowed to be below 0.
+		assertTrue(mage.getAmmo() >= 0);
+	}
+	@Test
+	public void testPrimaryAbility(){
+	Position pos = new Position(1,1);
+	int speed = 100;
+	Direction dir = Direction.EAST;
+	Team team = new Team("Test", Color.red, Side.LEFT );
+	Hero mage = new Mage(pos,speed,dir,team);
+	
+	mage.setAmmo(0);
+	Explosive testExplosive = mage.primaryAbility();
+	assertTrue(testExplosive == null);
+	
+	mage.setAmmo(1);
+	testExplosive = mage.primaryAbility();
+	assertTrue(mage.getAmmo() == 0);
+	
+	testExplosive.destroy();
+	assertTrue(mage.getAmmo() == 1);
+	
+	//Should test stun ammo as well.
+	}
+}
