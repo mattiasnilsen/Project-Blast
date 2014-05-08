2e22607197fc2804c2ee10e0af9a77892bf9921b
Atlanca <talex@student.chalmers.se>
2014-05-08 22:14:50 +0200

    Added new constant for Hero speed

--- src/projectblast/model/Constants.java
+++ src/projectblast/model/Constants.java
@@ -11,4 +11,6 @@ public final class Constants {
 	
 	public static final int TOWER_STARTING_HEALTH = 3;
 	public static final int TOWER_POWERUP_INTERVAL = 10 * FRAMERATE;
+	
+	public static final int HERO_START_SPEED = 0;
 }
