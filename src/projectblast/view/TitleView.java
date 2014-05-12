5725e80c05835e1c07c8170bac4a8bb87f021f10
Anton Freudenthaler <franton@student.chalmers.se>
2014-05-12 17:34:47 +0200

    Implementing TitleState & TitleView

--- src/projectblast/view/TitleView.java
+++ src/projectblast/view/TitleView.java
@@ -4,14 +4,18 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+import projectblast.model.ITitleModel;
+
 public class TitleView {
 
+	ITitleModel model;
 	Image playGame;
 	Image gameSettings;
 	Image exitGame;
 	Image arrow;
 	
-	public TitleView() {
+	public TitleView(ITitleModel model) {
+		this.model = model;
 	    try {
 			playGame = new Image("data/image/PlayGame.png");
 			gameSettings = new Image("data/image/GameSettings.png");
@@ -24,6 +28,11 @@ public class TitleView {
 	}
 	
 	public void render(Graphics g) {
+		g.drawString("Blasting fun!", 100, 50);
+		playGame.draw(100, 100);
+		gameSettings.draw(100, 200);
+		exitGame.draw(100, 300);
 		
+		arrow.draw(model.getSelectedColumn() + 400, (model.getSelectedRow() + 1)*100);
 	}
 }
