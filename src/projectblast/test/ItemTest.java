4db35e780c743b31c4793896c090d93c1b465832
Mattias Nilsen <kerp@live.se>
2014-05-15 19:53:50 +0200

    Added ItemTest.java

---
+++ src/projectblast/test/ItemTest.java
@@ -0,0 +1,51 @@
+package projectblast.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import projectblast.model.title.Item;
+
+public class ItemTest {
+
+    @Test
+    public void testItem() {
+        boolean caughtException = false;
+        try {
+            String nullString = null; //This needs to be done to differentiate between Item(String s) and Item(Item i)
+            new Item(nullString);
+        } catch(NullPointerException ex) {
+            caughtException = true;
+        }
+        assertTrue(caughtException);
+        
+        Item item = new Item("test");
+        assertTrue(item.getName().equals("test"));
+    }
+        
+
+    @Test
+    public void testCopyConstructor() {
+        boolean caughtException = false;
+        try {
+            Item nullItem = null; //This needs to be done to differentiate between Item(String s) and Item(Item i)
+            new Item(nullItem);
+        } catch(NullPointerException ex) {
+            caughtException = true;
+        }
+        assertTrue(caughtException);
+        
+        Item item = new Item("test");
+        Item other = new Item(item);
+        assertTrue(other.getName().equals("test"));
+    }
+
+    @Test
+    public void testSetSelected() {
+        Item item = new Item("test");
+        assertFalse(item.isSelected());
+        item.setSelected(true);;
+        assertTrue(item.isSelected());
+    }
+
+}
