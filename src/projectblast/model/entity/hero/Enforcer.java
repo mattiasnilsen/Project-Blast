fe47a1577a4a311d7527e5c5fdf5071670f4d0da
Axel Saven Östebo <axelostebo@gmail.com>
2014-05-22 14:03:12 +0200

    Enforcer now gets starter powerups like it should. Item no longer uses constructor to create a string.

--- src/projectblast/model/entity/hero/Enforcer.java
+++ src/projectblast/model/entity/hero/Enforcer.java
@@ -3,35 +3,48 @@ package projectblast.model.entity.hero;
 import projectblast.model.attribute.Direction;
 import projectblast.model.attribute.Position;
 import projectblast.model.attribute.Team;
+import projectblast.model.attribute.powerup.AmmoPowerUp;
+import projectblast.model.attribute.powerup.RangePowerUp;
+import projectblast.model.attribute.powerup.SpeedPowerUp;
 import projectblast.model.core.ICore;
 import projectblast.model.entity.Id;
+import projectblast.model.entity.explosive.Bomb;
+import projectblast.model.entity.explosive.Drone;
 import projectblast.model.entity.explosive.Explosive;
+import projectblast.model.helper.Constants;
 
 public class Enforcer extends Hero{
 
 	public Enforcer(Position position, Direction direction, Team team) {
 		super(position, direction, team);
 		setName(Id.ENFORCER);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void addInitialPowerUps() {
-		// TODO Auto-generated method stub
-		
+		addPowerUp(new SpeedPowerUp());
+		addPowerUp(new SpeedPowerUp());
+		addPowerUp(new SpeedPowerUp());
+		addPowerUp(new RangePowerUp());
+		addPowerUp(new AmmoPowerUp());
 	}
 
 	@Override
 	public Explosive primaryAbility() {
-		if(getAmmo() != 0){
+		if(getAmmo() <= 0){
 		return null;
+		} else {
+			setAmmo(getAmmo()-1);
+			Drone drone = new Drone(snapPosition(getPosition()), Constants.BOMB_SPEED, getDirection(), this);
+			addExplosive(drone);
+			return drone;
 		}
-		return null;
+		
 	}
 
 	@Override
 	public ICore secondaryAbility() {
-		// TODO Auto-generated method stub
+		//TODO Implement secondary
 		return null;
 	}
 
